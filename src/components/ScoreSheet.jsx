5b436493b8d1387c41f89b365c0e04a68bd792bc
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-11 17:52:33 -0600

    looked a an example of a input with multiple possibilities

--- src/components/ScoreSheet.jsx
+++ src/components/ScoreSheet.jsx
@@ -2,11 +2,35 @@ import React, { useState } from 'react'
 import TrebleClef from '../assets/Do_Mayor_armadura.svg'
 import './ScoreSheet.css'
 
-function ScoreSheet() {
-  const [isChecked, setIsChecked] = useState(false)
+import { toppings } from '../utils/toppings'
 
-  const handleOnChange = () => {
-    setIsChecked(!isChecked)
+const getFormattedPrice = (price) => `$${price.toFixed(2)}`
+
+function ScoreSheet() {
+  const [checkedState, setCheckedState] = useState(
+    new Array(toppings.length).fill(false)
+  )
+
+  const [total, setTotal] = useState(0)
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    )
+
+    setCheckedState(updatedCheckedState)
+
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + toppings[index].price
+        }
+        return sum
+      },
+      0
+    )
+
+    setTotal(totalPrice)
   }
 
   return (
@@ -24,21 +48,41 @@ function ScoreSheet() {
                 className="treble-clef"
               />
               <div>
-                Select your pizza topping:
-                <div className="topping">
-                  <input
-                    type="checkbox"
-                    id="topping"
-                    name="topping"
-                    value="Paneer"
-                    checked={isChecked}
-                    onChange={handleOnChange}
-                  />
-                  Paneer
-                </div>
-                <div className="result">
-                  Above checkbox is {isChecked ? 'checked' : 'un-checked'}.
-                </div>
+                <h3>Select Toppings</h3>
+                <ul className="toppings-list">
+                  {toppings.map(({ name, price }, index) => {
+                    return (
+                      <li key={index}>
+                        <div className="toppings-list-item">
+                          <div className="left-section">
+                            <input
+                              type="checkbox"
+                              id={`custom-checkbox-${index}`}
+                              name={name}
+                              value={name}
+                              checked={checkedState[index]}
+                              onChange={() => handleOnChange(index)}
+                            />
+                            <label htmlFor={`custom-checkbox-${index}`}>
+                              {name}
+                            </label>
+                          </div>
+                          <div className="right-section">
+                            {getFormattedPrice(price)}
+                          </div>
+                        </div>
+                      </li>
+                    )
+                  })}
+                  <li>
+                    <div className="toppings-list-item">
+                      <div className="left-section">Total:</div>
+                      <div className="right-section">
+                        {getFormattedPrice(total)}
+                      </div>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
