adf16a79cb8b543170f74942e19ac91e795fc3cd
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-11 12:46:50 -0600

    styled the score sheet

--- src/components/ScoreSheet.jsx
+++ src/components/ScoreSheet.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import TrebleClef from '../assets/Do_Mayor_armadura.svg'
+import './ScoreSheet.css'
 
 function ScoreSheet() {
   return (
     <>
-      <img src={TrebleClef} alt="treble clef svg" />
+      <section>
+        <header id="scoreheader">
+          <h3>Your title goes here</h3>
+        </header>
+        <img src={TrebleClef} alt="treble clef svg" />
+      </section>
     </>
   )
 }
