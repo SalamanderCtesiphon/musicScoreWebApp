1b49304c0b4475bc625957dc53d544307d9e760b
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-11 18:11:39 -0600

    reverted back to a previous design trying to determine the best ui and input method for app as well how to update and store the state

--- src/components/ScoreSheet.jsx
+++ src/components/ScoreSheet.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import TrebleClef from '../assets/Do_Mayor_armadura.svg'
 import './ScoreSheet.css'
 
@@ -17,25 +17,44 @@ function ScoreSheet() {
                 alt="treble clef svg"
                 className="treble-clef"
               />
-              <div className="eighth-note-section">
-                <div>
-                  <input type="checkbox" />
-                  <label htmlFor="cNote">C</label>
-                </div>
-                <div>
-                  <input type="checkbox" />
-                  <label htmlFor="cNote">C</label>
-                </div>
-                <div>
-                  <input type="checkbox" />
-                  <label htmlFor="cNote">C</label>
-                </div>
-                <div>
-                  <input type="checkbox" />
-                  <label htmlFor="cNote">C</label>
-                </div>
-              </div>
+              <div className="music-score-line1"></div>
             </div>
+            <div className="text-line"></div>
+          </div>
+          <div className="score-sheet">
+            <div className="top-line">
+              <img
+                src={TrebleClef}
+                alt="treble clef svg"
+                className="treble-clef"
+              />
+              <div className="music-score-line1"></div>
+            </div>
+            <div className="text-line"></div>
+          </div>
+          <div className="score-sheet">
+            <div className="top-line">
+              <img
+                src={TrebleClef}
+                alt="treble clef svg"
+                className="treble-clef"
+                id="treble-clef3"
+              />
+              <div className="music-score-line1"></div>
+            </div>
+            <div className="text-line"></div>
+          </div>
+          <div className="score-sheet">
+            <div className="top-line">
+              <img
+                src={TrebleClef}
+                alt="treble clef svg"
+                className="treble-clef"
+                id="treble-clef4"
+              />
+              <div className="music-score-line1"></div>
+            </div>
+            <div className="text-line"></div>
           </div>
         </div>
       </section>
